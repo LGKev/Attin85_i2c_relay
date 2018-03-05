35d88aa221fb91c716d93e93727ec9c5eda8fd3e
U-SPARKFUN\kevin.kuwata <kevin.kuwata@SX-KEVIN.sparkfun.local>
2018-03-05 13:00:15 -0700

    adding more checks for the ISR parsing the commands from master

--- Slave_i2c_attin85.ino
+++ Slave_i2c_attin85.ino
@@ -34,7 +34,7 @@ volatile bool relay_state = false; //default off;
 volatile byte new_address; //latest address for the slave.
 
 byte registerMap[REGISTER_MAP_SIZE];
-volatile byte receievedCommands[MAX_BYTES_RECEIVED];
+volatile byte receivedCommands[MAX_BYTES_RECEIVED];
 
 
 //ISR prototypes
@@ -45,13 +45,13 @@ void setup() {
 
   TinyWire.begin(0x01);                // join i2c bus with address #1
   TinyWire.onReceive(receiveEvent); // register event
-  TinyWire.onRequest(requestEvent);  
+  TinyWire.onRequest(requestEvent);
   pinMode(RELAY_PIN, OUTPUT);
-  
-  registerMap[1] = 0; //Relay NC 
+
+  registerMap[1] = 0; //Relay NC
   registerMap[2] = 0x00; // all cleared
   new_address = SLAVE_ADDRESS;
-    digitalWrite(LED, LOW); //only place the led is turned on is in the ONReceive ISR. and turned off here. 
+  digitalWrite(LED, LOW); //only place the led is turned on is in the ONReceive ISR. and turned off here.
 }
 
 void loop() {
@@ -61,14 +61,14 @@ void loop() {
     //update the stuff
     update(); // updates the relay state only.
     update_register = 0; //reset flag`
-  
+
 #ifdef DEBUG_OUTPUT
-  Serial.print("the address of the slave is:  ");
-  Serial.println(new_address);
-  
-  Serial.print("the status register is:  ");
-  byte statusBitStream = registerMap[2];
-  Serial.println(statusBitStream, BIN);
+    Serial.print("the address of the slave is:  ");
+    Serial.println(new_address);
+
+    Serial.print("the status register is:  ");
+    byte statusBitStream = registerMap[2];
+    Serial.println(statusBitStream, BIN);
 #endif
   }
   //check here the state of the relay, in register map
@@ -78,13 +78,13 @@ void loop() {
     //TODO: add the status register update here.
     digitalWrite(RELAY_PIN, HIGH);
     digitalWrite(LED, HIGH);
-  registerMap[2] |= BIT0;
+    registerMap[2] |= BIT0;
   }
   if (registerMap[1] == 0) {
     //TODO: add the status register update here
     digitalWrite(RELAY_PIN, LOW);
     digitalWrite(LED, LOW);
-  registerMap[2] &= ~BIT0; //this should make bit0 in the map[2] to be a 0.
+    registerMap[2] &= ~BIT0; //this should make bit0 in the map[2] to be a 0.
   }
 }
 
@@ -93,59 +93,59 @@ void loop() {
 /*========================================================*/
 /*
     @brief: Parses the commands received to determine how the
-      slave device should behave. this is where the address 
+      slave device should behave. this is where the address
       changes, and where the state of the relay changes
       DOES NOT update the actual relay output here, that is done in
       the main loop.
     @input: *global* receivedCommands
     @returns: nothing
-    
+
     @flags:  update_register flag to 1
 */
 void relayConfig() {
   //now we have collected info, now we need to parse it.
   // use a switch statement to change based on the command
 
-  switch (receievedCommands[0]) {
+  switch (receivedCommands[0]) {
     case 0x03: //change slave address
       update_register = 1;
-      new_address = receievedCommands[1];
- #ifdef DEBUG_OUTPUT
-  Serial.print("The newest address is: ");
-  Serial.println(new_address);
-    for(int i=0; i< 10; i++){
-      digitalWrite(13, HIGH);
-      delay(75);
-      digitalWrite(13, LOW);
-      delay(75); 
+      new_address = receivedCommands[1];
+#ifdef DEBUG_OUTPUT
+      Serial.print("The newest address is: ");
+      Serial.println(new_address);
+      for (int i = 0; i < 10; i++) {
+        digitalWrite(13, HIGH);
+        delay(75);
+        digitalWrite(13, LOW);
+        delay(75);
       }
- #endif
-//QUESTION: what would happen if this was left out?, especially
-  //if the number of bytes expected is larger than 2.
-//      bytesReceived--;
-//      if (bytesReceived == 1) {
-//        return; // only expecting 2 bytes
-//      }
-    break;
+#endif
+      //QUESTION: what would happen if this was left out?, especially
+      //if the number of bytes expected is larger than 2.
+      //      bytesReceived--;
+      //      if (bytesReceived == 1) {
+      //        return; // only expecting 2 bytes
+      //      }
+      break;
     case 0x01: //change relay state: on or off
       //next byte is the state, 1 is on, 0 is off.
-      relay_state = receievedCommands[1];
+      relay_state = receivedCommands[1];
       update_register = 1;
-//      bytesReceived--;
-//      if (bytesReceived == 1) {
-//        return; // only expecting 2 bytes
-//      }
-    break;
+      //      bytesReceived--;
+      //      if (bytesReceived == 1) {
+      //        return; // only expecting 2 bytes
+      //      }
+      break;
     default:
       //trying to write to a READ-ONLY register.
       digitalWrite(13, LOW);
-    update_register = 0;
+      update_register = 0;
       return;// out of bounds
   }
 }
 
 /*
-    @brief: Sets i2c address being used, updates the 
+    @brief: Sets i2c address being used, updates the
       registerMap for relay state on or off
     @input: none, *global slave address*
     @returns: none
@@ -165,7 +165,7 @@ void update() {
 /*========================================================*/
 /*
     @brief: When the master initiates a command and data to slave
-    @input: bytesReceieved, to compare how many bytes received, 
+    @input: bytesReceieved, to compare how many bytes received,
       with how many bytes expected. Only keep the number of expected
       and then lose the rest.
     @returns: none
@@ -173,21 +173,34 @@ void update() {
 */
 void receiveEvent(int bytesReceived) {
 
-// digitalWrite(LED, HIGH);
- //delay(200);
-// digitalWrite(LED, LOW);
- //delay(200);
-//
-//  this is where the bug lies. 
+  // digitalWrite(LED, HIGH);
+  //delay(200);
+  // digitalWrite(LED, LOW);
+  //delay(200);
+  //
+  //  this is where the bug lies.
   for (int i = 0; i < bytesReceived; i++) {
     //loop through the data from the master
     if (i < MAX_BYTES_RECEIVED) {
-      receievedCommands[i] = TinyWire.read(); //all commands and data are collected in the ISR... do not process here.
+      receivedCommands[i] = TinyWire.read(); //all commands and data are collected in the ISR... do not process here.
     }
     else {
       TinyWire.read(); // let them come but don't collect
     }
   }
+  //case 1: master sets address to read from
+  //case 3: master sets address to write more than one byte of data, conseq addressing
+  //case 2: master sets the address to write a single bye of data.
+
+  if (bytesReceived == 1 && (receivedCommands[0] < REGISTER_MAP_SIZE)) {
+    return;
+  }
+  if (bytesReceived == 1 && (receivedCommands[0] >= REGISTER_MAP_SIZE)) {
+    receivedCommands[0] = 0x00;
+    return;
+  }
+
+
 }// end of receive ISR
 
 
@@ -199,11 +212,11 @@ void receiveEvent(int bytesReceived) {
     @flags:  none
 */
 void requestEvent() {
-	
-//  TinyWire.write(registerMap, REGISTER_MAP_SIZE);
-//  TinyWire.send(registerMap[0]);
-//  TinyWire.send(registerMap[1]);
-//  we will send entire map, but we only need to
-//   send the status, so bit shift?
+
+  //  TinyWire.write(registerMap, REGISTER_MAP_SIZE);
+  //  TinyWire.send(registerMap[0]);
+  //  TinyWire.send(registerMap[1]);
+  //  we will send entire map, but we only need to
+  //   send the status, so bit shift?
 }// end of request ISR
 
