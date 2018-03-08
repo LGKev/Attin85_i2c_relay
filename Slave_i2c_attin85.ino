33598ba767131891c08e11ea91b10c9f1624c13f
U-SPARKFUN\kevin.kuwata <kevin.kuwata@SX-KEVIN.sparkfun.local>
2018-03-07 17:36:58 -0700

    basic functionality of the on or off and reduced the trash in the code

--- Slave_i2c_attin85.ino
+++ Slave_i2c_attin85.ino
@@ -5,36 +5,21 @@
 
 #include <TinyWire.h> //https://github.com/lucullusTheOnly/TinyWire
 
-//#define DEBUG_OUTPUT
-#define BIT0    0b00000001
-#define BIT1    0b00000010
 
 
 #define RELAY_PIN   4
 #define LED         0 // on board led for the Attiny programmer
 
-#define MAX_BYTES_RECEIVED 3 //we only are sending to turn ON, OFF, STATUS
-// I think its 3 bytes because, you first send address
-// then you send register you want to talk to
-// then you send the value you want to send.
 
-#define REGISTER_MAP_SIZE   3// ADDRESS, STATUS, ON
-#define SLAVE_ADDRESS   0x01//1 //whats a good way to choose?
+#define REGISTER_MAP_SIZE    3// ADDRESS, STATUS, ON
+#define SLAVE_ADDRESS     0x18
 
 
-//Address Map for COMMANDS
-#define TURN_ON_REG     (0X01)
-#define STATUS_REG      (0X02)
-
+volatile int ReceivedData[32]; //32 byte array to act as a buffer for I2C data. 32 bytes is the max for an UNO 
 
 //Control Flags
 volatile bool update_register = false;
 volatile bool relay_state = false; //default off;
-
-volatile byte new_address; //latest address for the slave.
-
-byte registerMap[REGISTER_MAP_SIZE];
-volatile byte receivedCommands[MAX_BYTES_RECEIVED];
 
 
 //ISR prototypes
@@ -43,125 +28,27 @@ void receiveEvent(void);
 
 void setup() {
 
-  TinyWire.begin(0x01);                // join i2c bus with address #1
-  TinyWire.onReceive(receiveEvent); // register event
-  TinyWire.onRequest(requestEvent);
-  pinMode(RELAY_PIN, OUTPUT);
-
-  registerMap[1] = 0; //Relay NC
-  registerMap[2] = 0x00; // all cleared
-  new_address = SLAVE_ADDRESS;
-  digitalWrite(LED, LOW); //only place the led is turned on is in the ONReceive ISR. and turned off here.
+  TinyWire.begin(SLAVE_ADDRESS);
+  //TODO: eventually check eeprom, then default    
+    pinMode(RELAY_PIN, OUTPUT);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW); //only place the led is turned on is in the ONReceive ISR. and turned off here.
+    TinyWire.onReceive(receiveEvent); // register event
+    TinyWire.onRequest(requestEvent);
 }
 
 void loop() {
-
-   digitalWrite(RELAY_PIN, receivedCommands[1]);
-
-//  
-//  relayConfig();
-//  //check the flags ... polling?
-//  if (update_register == 1) {
-//    //update the stuff
-//    update(); // updates the relay state only.
-//    update_register = 0; //reset flag`
-//
-//#ifdef DEBUG_OUTPUT
-//    Serial.print("the address of the slave is:  ");
-//    Serial.println(new_address);
-//
-//    Serial.print("the status register is:  ");
-//    byte statusBitStream = registerMap[2];
-//    Serial.println(statusBitStream, BIN);
-//#endif
-//  }
-//  //check here the state of the relay, in register map
-//  // the following code can probably be a nice function.
-//  // set relay STATE accordingly.
-//  if (registerMap[1] == 1) {
-//    //TODO: add the status register update here.
-//    digitalWrite(RELAY_PIN, HIGH);
-//    digitalWrite(LED, HIGH);
-//    registerMap[2] |= BIT0;
-//  }
-//  if (registerMap[1] == 0) {
-//    //TODO: add the status register update here
-//    digitalWrite(RELAY_PIN, LOW);
-//    digitalWrite(LED, LOW);
-//    registerMap[2] &= ~BIT0; //this should make bit0 in the map[2] to be a 0.
-//  }
+	if(ReceivedData[0] == 0x01){
+		digitalWrite(RELAY_PIN, HIGH);
+	}
+	if(ReceivedData[0] == 0x00){
+		digitalWrite(RELAY_PIN, LOW);
+	}
 }
 
 /*========================================================*/
 //        Helper Functions
 /*========================================================*/
-/*
-    @brief: Parses the commands received to determine how the
-      slave device should behave. this is where the address
-      changes, and where the state of the relay changes
-      DOES NOT update the actual relay output here, that is done in
-      the main loop.
-    @input: *global* receivedCommands
-    @returns: nothing
-
-    @flags:  update_register flag to 1
-*/
-void relayConfig() {
-  //now we have collected info, now we need to parse it.
-  // use a switch statement to change based on the command
-
-  switch (receivedCommands[0]) {
-    case 0x03: //change slave address
-      update_register = 1;
-      new_address = receivedCommands[1];
-#ifdef DEBUG_OUTPUT
-      Serial.print("The newest address is: ");
-      Serial.println(new_address);
-      for (int i = 0; i < 10; i++) {
-        digitalWrite(13, HIGH);
-        delay(75);
-        digitalWrite(13, LOW);
-        delay(75);
-      }
-#endif
-      //QUESTION: what would happen if this was left out?, especially
-      //if the number of bytes expected is larger than 2.
-      //      bytesReceived--;
-      //      if (bytesReceived == 1) {
-      //        return; // only expecting 2 bytes
-      //      }
-      break;
-    case 0x01: //change relay state: on or off
-      //next byte is the state, 1 is on, 0 is off.
-      relay_state = receivedCommands[1];
-      update_register = 1;
-      //      bytesReceived--;
-      //      if (bytesReceived == 1) {
-      //        return; // only expecting 2 bytes
-      //      }
-      break;
-    default:
-      //trying to write to a READ-ONLY register.
-      digitalWrite(13, LOW);
-      update_register = 0;
-      return;// out of bounds
-  }
-}
-
-/*
-    @brief: Sets i2c address being used, updates the
-      registerMap for relay state on or off
-    @input: none, *global slave address*
-    @returns: none
-    @flags:  none
-*/
-void update() {
-  //write to the memory register. status register?
-  TinyWire.begin(new_address);
-  registerMap[1] = relay_state;
-}
-
-
 
 
 /*========================================================*/
@@ -169,25 +56,19 @@ void update() {
 /*========================================================*/
 /*
     @brief: When the master initiates a command and data to slave
-    @input: bytesReceieved, to compare how many bytes received,
-      with how many bytes expected. Only keep the number of expected
-      and then lose the rest.
+		ie) the master says 0x01, then sends a 1, means command: 0x01 then the slave listens for the next thing, which is the relay state 1
+    @global: This is modifying ReceivedData array, which stores all the bytes that a master sent to the slave.
+	@input:
     @returns: none
     @flags:  none
 */
 void receiveEvent(int bytesReceived) {
-  //bug is in this ISR
+	int index = 0; //index of all the data received from master.
+	
   while(TinyWire.available() > 0){ //this was the solution, 
-    if(TinyWire.read() == 0x01){ //then doing this to capture what value we are actually writing. 
-      //how would this change if we had many registers which we are writing? i guess we
-      // would do a while loop for the data stream.
-      //then do a case statement on what was read, and if its 0x01 its the ON register
-    //  if 0x02 its the address register
-    //  0x0 3 its the status register.
-      //thats when we would need the case 1 2 and 3 to decide if where we are saying to write is valid. 
-      receivedCommands[1] = TinyWire.read();
-    }
-  }
+		ReceivedData[index] = TinyWire.read();
+		index++;
+  }//end of the while loop, data available
 }// end of receive ISR
 
 
